2b3d9d34b0e1a7eed196d677e18424613821a6b7
Hossam Elbatal <tokyo.hossam@gmail.com>
2024-11-10 10:16:01 +0200

    finished and tested basic logic functionality on the frontend

---
+++ frontend-desktop/src/components/Login.jsx
@@ -0,0 +1,76 @@
+import { React, useState } from 'react';
+import axios from 'axios';
+
+
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await axios.post(
+            'http://localhost:3000/api/login',
+            { email, password },
+            {
+            withCredentials: true, 
+            }
+        );
+
+      console.log('Login successful:', response.data);
+      alert('Login successful!'); 
+
+      setEmail('');
+      setPassword('');
+    } catch (error) {   
+      console.error('Error during login:', error.response || error.message);
+      setErrorMessage('Login failed. Please check your credentials');
+    }
+  };
+
+
+  return (
+    <div className="form-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+        />
+        </div>
+        <div className="input-group">
+        <label htmlFor="password">Password</label>
+        <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+        />
+        </div>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+        <div className="button-group">
+        <button type="submit" className="login-btn">Login</button>
+        <button
+            type="button"
+            className="register-btn"
+        >
+            Register
+        </button>
+        </div>
+    </form>
+    </div>
+);
+}
+
+export default Login;
